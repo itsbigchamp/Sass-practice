98880d1acd5f345658546292a91139b9cb430516
Gavin Anderson <btcgrowth1@gmail.com>
2022-08-11 17:08:02 -0400

    Sass-practice

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+
+
+const Header = () => {
+  return (
+    <>
+      <ul className="navigation">
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Contact</a>
+        </li>
+        <li>
+          <a href="">Links</a>
+        </li>
+      </ul>
+
+      <div className="menu">
+        <a href="" className="menu__link">Link1</a>
+        <a href="" className="menu__link">Link2</a>
+        <a href="" className="menu__link">Link3</a>
+      </div>
+    </>
+  );
+};
+
+export default Header;
